2f48c9127e335ef2e978676fc6a2df58790165cd
E. Almqvist <elalmqvist@gmail.com>
2021-03-01 20:25:51 +0100

    JS bugs

--- js/scroll_effect.js
+++ js/scroll_effect.js
@@ -4,10 +4,29 @@
 // Yes. This could be considered as bloat. But if you do not like it then disable JavaScript in your browser.
 
 const checkpoint_y = 200;
+const elemSections = document.getElementsByTagName("section");
+
+console.log(elemSections);
+
+function elementOffsetY(elem) {
+	var rect = elem.getBoundingClientRect();
+	
+	scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	return scrollTop + rect.top;
+}
+
+function calculateOpacity(elem, scroll_y) { return 1 - (scroll_y / elementOffsetY(elem)); }
 
 window.addEventListener("scroll", () => {
-	const scroll_y = window.pageYOffset;
-	alpha = 1 - (scroll_y / checkpoint_y);
+	const scroll_y = window.pageYOffset; // current Y-coordinate
+	// alpha = 1 - (scroll_y / checkpoint_y); // calculate opacity for said element
 
-	document.querySelector("section").style.opacity = alpha;
+	//document.querySelector("section").style.opacity = alpha; // apply opacity to said element
+	var alphaBuffer;
+
+	elemSections.forEach( function(elem) {
+		alphaBuffer = calculateOpacity(elem, scroll_y);
+		elem.style.opacity = alphaBuffer;
+		console.log(alphaBuffer);
+	});
 });
