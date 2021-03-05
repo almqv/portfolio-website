38f7cb44f5e7de2ca8bbd3ec6e7022bb80f75fc7
E. Almqvist <elalmqvist@gmail.com>
2021-03-05 15:59:36 +0100

    Observer

--- js/scroll_effect.js
+++ js/scroll_effect.js
@@ -4,37 +4,19 @@
 
 // Yes. This could be considered as bloat. But if you do not like it then disable JavaScript in your browser.
 
-const distanceRange = 150;
 const elemSections = document.getElementsByTagName("section"); // list of all the sections
 
-function replaceClass(elem, cl, newcl) {
-	elem.className = elem.className.replace(/\bcl\b/g, newcl);
+let options = {
+	root: null,
+	rootMargin: "0px",
+	threshold: [0, 0.5]
 }
 
-function elementInViewport(elem, fraction) {
-	let rect = elem.getBoundingClientRect();
-	let viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+function test(a, b) {
+	console.log(a, b, "AHIOdfvchoisdhfiosdhfio", count);
+}
 
-	topOffset = rect.top >= 0 ? 0 : rect.top;
-	topFraction = Math.floor(1.0 - (topOffset / rect.height));
+let sectionsCount = elemSections.length;
+let observer = new IntersectionObserver(test, options);
 
-	bottomOffset = rect.bottom - viewportHeight;
-	bottomFraction = Math.floor( 1.0 - (bottomOffset / rect.height) );
-
-	return !(topFraction < fraction || bottomFraction < fraction);
-};
-
-
-window.addEventListener("scroll", () => {
-	//const scroll_y = window.pageYOffset; // current Y-coordinate
-	
-	var visibleBuffer;
-	var elemBuffer;
-
-	for (let i = 0; i < elemSections.length; i++) {
-		elemBuffer = elemSections[i];
-
-		visibleBuffer = elementInViewport(elemBuffer, 0.2);
-		console.log(elemBuffer.id, visibleBuffer);
-	}
-});
+for( let i = 0; i < sectionsCount; i++ ) { observer.observe(elemSections[i]); } // observe all of the sections
