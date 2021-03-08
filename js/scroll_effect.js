bb2aec45d94d91303411de8ebf927bda03b51838
E. Almqvist <elalmqvist@gmail.com>
2021-03-08 10:27:23 +0100

    Animation fix for fadein/out

--- js/scroll_effect.js
+++ js/scroll_effect.js
@@ -5,18 +5,37 @@
 // Yes. This could be considered as bloat. But if you do not like it then disable JavaScript in your browser.
 
 const elemSections = document.getElementsByTagName("section"); // list of all the sections
+const intRatioLimit = 0.25;
 
 let options = {
 	root: null,
-	rootMargin: "0px",
-	threshold: [0, 0.5]
+	rootMargin: "-40% 0px 0px 0px", // TODO: needs to be relative to content not wrapper
+	threshold: [0, intRatioLimit]
 }
 
-function test(a, b) {
-	console.log(a, b, "AHIOdfvchoisdhfiosdhfio");
+function toggleAnimation(elem, intRatio) {
+	console.log("Does this work?", elem, intRatio);
+	if( intRatio >= intRatioLimit ) {
+		// fade in
+		elem.classList.remove("anim_fadeout");
+		elem.classList.add("anim_fadein");
+	} else {
+		// fade out
+		elem.classList.remove("anim_fadein");
+		elem.classList.add("anim_fadeout");
+	}	
+}
+
+function handleIntersection(intEntry, _) {
+	for( let section of intEntry ) {
+		toggleAnimation(section.target, section.intersectionRatio);
+	}
 }
 
 let sectionsCount = elemSections.length;
-let observer = new IntersectionObserver(test, options);
+let observer = new IntersectionObserver(handleIntersection, options);
 
-for( let i = 0; i < sectionsCount; i++ ) { observer.observe(elemSections[i]); } // observe all of the sections
+for (let section of elemSections) { 
+	observer.observe(section); 
+	section.classList.add("anim_fadeout");
+} // observe all of the sections
